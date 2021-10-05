7e4a64cd70dabae9015b07abdff83adcd3b52289
Aiden Gu <aiden.gu01@gmail.com>
2021-10-06 01:28:31 +1100

    Add background coloring and simple navbar header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Components/Navbar"
 
 function App() {
   return (
     <div className="App">
+      <Navbar /> 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hey <code>peeps</code> 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="container">
+      </div>
     </div>
   );
 }
