65ae4cefd34928218ca646a5cbd1edeacb8354bd
Aiden Gu <aiden.gu01@gmail.com>
2021-10-08 12:46:31 +1100

    Reconfigure eslintrc.json to include extend 'airbnb-base' to remove 'no definition for extraneous imports'. Restructure code to follow eslintrc styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import './App.css';
-import Navbar from "./Components/Navbar/"
+import Navbar from './Components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar /> 
-      <header className="App-header">
-        <p>
-          Hey <code>peeps</code> 
-        </p>
-      </header>
-      <div className="container">
-      </div>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div className="App">
+    <Navbar />
+    <header className="App-header">
+      <p>
+        Hey <code>peeps</code>
+      </p>
+    </header>
+  </div>
+);
 
 export default App;
--- client/src/Components/Navbar/index.tsx
+++ client/src/Components/Navbar/index.tsx
@@ -1,16 +1,20 @@
- import redbacklogo from "../../assets/redbacklogo.png"
-import "./index.css"
+import redbacklogo from '../../assets/redbacklogo.png';
+import './index.css';
 
-const Navbar = () => {
-    return (
-        <div className="container-fluid bg-dark">
-            <nav className="navbar navbar-light bg-dark">
-                <div className="redbacklogowrapper"> 
-                    <img className="img-fluid" src={redbacklogo} height="50" width="50" alt="redbacklogo" />
-                </div>
-            </nav>
-        </div>
-    )
-}
+const Navbar: React.FunctionComponent = () => (
+  <div className="container-fluid bg-dark">
+    <nav className="navbar navbar-light bg-dark">
+      <div className="redbacklogowrapper">
+        <img
+          className="img-fluid"
+          src={redbacklogo}
+          height="50"
+          width="50"
+          alt="redbacklogo"
+        />
+      </div>
+    </nav>
+  </div>
+);
 
-export default Navbar; 
+export default Navbar;
