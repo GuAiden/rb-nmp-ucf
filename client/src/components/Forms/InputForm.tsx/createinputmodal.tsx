c464c75f727f6b70d78daf0933b2f6e53ad678a5
Aiden Gu <aiden.gu01@gmail.com>
2021-10-11 00:19:59 +1100

    Added modal styling

--- client/src/components/Forms/InputForm.tsx/createinputmodal.tsx
+++ client/src/components/Forms/InputForm.tsx/createinputmodal.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
+import './index.css'
 
 /**
  * @returns A modal component that opens to handle form input for the 'inputs' page
@@ -22,19 +23,45 @@ const CreateInputModal: React.FC = () => {
         </Button>
       </div>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header>
+      <Modal show={show} onHide={handleClose} centered>
+        {/* <Modal.Header className="modal-wrapper input-modal">
           <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
+        </Modal.Header> */}
+        <Modal.Body className="input-modal">
+          <div className="container">
+            <div className="row">
+              <header className="modal-header-wrapper">CREATE NEW INPUT</header>
+            </div>
+            <div className="row">
+              <text>Form inputs soon</text>
+            </div>
+            <div className="row justify-content-end">
+              <div className="col-2">
+                <Button
+                  variant="secondary"
+                  size="lg"
+                  onClick={handleClose}
+                  className="modal-add-wrapper"
+                >
+                  Add
+                </Button>
+              </div>
+              <div className="col-2">
+                <Button variant="primary" size="lg" onClick={handleClose}>
+                  Save
+                </Button>
+              </div>
+            </div>
+          </div>
+        </Modal.Body>
+        {/* <Modal.Footer className="input-modal">
+          <Button variant="secondary" size="lg" onClick={handleClose}>
+            Add
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
+          <Button variant="primary" size="lg" onClick={handleClose}>
+            Save
           </Button>
-        </Modal.Footer>
+        </Modal.Footer> */}
       </Modal>
     </>
   );
