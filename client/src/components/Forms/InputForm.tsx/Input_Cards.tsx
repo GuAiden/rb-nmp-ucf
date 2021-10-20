f1373326acd8d2dfaac0144212007ef3d63b8d06
Aiden Gu <aiden.gu01@gmail.com>
2021-10-20 16:31:06 +1100

    Add most of the functionality for input list cards, now just have to add in conversion/output conditional logic

--- client/src/components/Forms/InputForm.tsx/Input_Cards.tsx
+++ client/src/components/Forms/InputForm.tsx/Input_Cards.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import { Input } from '../Input_Types';
+import './Input_Cards.css';
 
 type InputCardsProps = {
   inputList: Input[];
@@ -10,11 +11,30 @@ const InputCards: React.FunctionComponent<InputCardsProps> = ({
   inputList,
 }: InputCardsProps) => (
   <Container className="p-5">
-    <Row md={3} className="g-5">
-      {inputList.map(() => (
-        <Col className="text-left">
-          <Card bg="secondary">
-            <text className="text-left">hello</text>
+    <Row md={3} className="g-5 justify-content-start text-left">
+      {inputList.map((_, idx) => (
+        <Col>
+          <Card bg="secondary" className="card-wrapper">
+            <Card.Body>
+              <Row>
+                <text className="card-text">Channel Name</text>
+              </Row>
+              <Row>
+                <text className="input-text">{inputList[idx].channelName}</text>
+              </Row>
+              <Row className="pt-2">
+                <Col>
+                  <text className="card-text">Channel Number</text>
+                  <text className="input-text">
+                    {inputList[idx].channelNumber}
+                  </text>
+                </Col>
+                <Col>
+                  <text className="card-text">Units</text>
+                  <text className="input-text">{inputList[idx].units}</text>
+                </Col>
+              </Row>
+            </Card.Body>
           </Card>
         </Col>
       ))}
