f4bec879c0455b68a583b7303f8ab32a93bc0d04
Aiden Gu <aiden.gu01@gmail.com>
2021-10-30 02:49:18 +1100

    Remove unwanted 'shallow' imports from CreateInputMOdal.test files

--- client/src/tests/CreateInputModal.test.tsx
+++ client/src/tests/CreateInputModal.test.tsx
@@ -1,4 +1,4 @@
-import Enzyme, { mount, shallow } from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import CreateInputModal from '../components/Forms/InputForm/Create_Input_Modal';
 import { Input } from '../components/Forms/Input_Types';
